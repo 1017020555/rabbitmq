c9e4ddaeba1295bd699f788cd68bb6a845e2c662
lc <1017020555@qq.com>
2020-05-17 22:01:37 +0800

    fanout,direct,topic

--- src/main/java/com/lc/rabbitmq/direct/TestProducter.java
+++ src/main/java/com/lc/rabbitmq/direct/TestProducter.java
@@ -1,13 +1,13 @@
-package com.lc.rabbitmq.fanout;
+package com.lc.rabbitmq.direct;
 
 import com.lc.rabbitmq.utils.RabbitMQUtil;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 
-public class TestProducer {
-  public final static String EXCHANGE_NAME="fanout_exchange";
-    public static void main(String[] args) throws Exception{
+public class TestProducter {
+  public final static String QUEUE_NAME="direct_queue";
+    public static void main(String[] args) throws  Exception{
         RabbitMQUtil.checkServer();
 
         //创建连接工厂
@@ -18,13 +18,11 @@ public class TestProducer {
         Connection connection = factory.newConnection();
         //创建一个通道
         Channel channel = connection.createChannel();
-//      交换机名称；交换机类型
-        channel.exchangeDeclare(EXCHANGE_NAME, "fanout");
 
         for (int i = 0; i < 100; i++) {
             String message = "direct 消息 " +i;
             //发送消息到队列中
-            channel.basicPublish(EXCHANGE_NAME, "", null, message.getBytes("UTF-8"));
+            channel.basicPublish("", QUEUE_NAME, null, message.getBytes("UTF-8"));
             System.out.println("发送消息： " + message);
 
         }
--- src/main/java/com/lc/rabbitmq/topic/TestCustomer4News.java
+++ src/main/java/com/lc/rabbitmq/topic/TestCustomer4News.java
@@ -1,16 +1,17 @@
-package com.lc.rabbitmq.fanout;
+package com.lc.rabbitmq.topic;
 
-import cn.hutool.core.util.RandomUtil;
 import com.lc.rabbitmq.utils.RabbitMQUtil;
 import com.rabbitmq.client.*;
 
 import java.io.IOException;
+import java.util.concurrent.TimeoutException;
 
-public class TestConsumer {
-  public final static String EXCHANGE_NAME="fanout_exchange";
-    public static void main(String[] args) throws Exception{
-        //为当前消费者取随机名
-        String name = "consumer-"+ RandomUtil.randomString(5);
+public class TestCustomer4News {
+    public final static String EXCHANGE_NAME="topics_exchange";
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        //为当前消费者取名称
+        String name = "consumer-news";
 
         //判断服务器是否启动
         RabbitMQUtil.checkServer();
@@ -23,12 +24,12 @@ public class TestConsumer {
         //创建一个通道
         Channel channel = connection.createChannel();
         //交换机声明（参数为：交换机名称；交换机类型）
-        channel.exchangeDeclare(EXCHANGE_NAME,"fanout");
+        channel.exchangeDeclare(EXCHANGE_NAME,"topic");
         //获取一个临时队列
         String queueName = channel.queueDeclare().getQueue();
-        //队列与交换机绑定（参数为：队列名称；交换机名称；routingKey忽略）
-        channel.queueBind(queueName,EXCHANGE_NAME,"");
+        //接受 USA 信息
 
+        channel.queueBind(queueName, EXCHANGE_NAME, "*.news");
         System.out.println(name +" 等待接受消息");
         //DefaultConsumer类实现了Consumer接口，通过传入一个频道，
         // 告诉服务器我们需要那个频道的消息，如果频道中有消息，就会执行回调函数handleDelivery
@@ -42,7 +43,6 @@ public class TestConsumer {
             }
         };
         //自动回复队列应答 -- RabbitMQ中的消息确认机制
-//        队列名，是否自动接受，消费者
         channel.basicConsume(queueName, true, consumer);
     }
 }
